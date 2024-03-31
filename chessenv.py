afa56f4cdfb03c2c021f1ab43f91cc8079bcf907
woodphrog <marinezra02@gmail.com>
2024-03-31 16:28:49 -0400

    still broken

--- chessenv.py
+++ chessenv.py
@@ -3,6 +3,7 @@ import evaluate
 import chess
 import numpy
 from typing import Union
+import math
 
 class chessEnv:
     def __init__(self, board: chess.Board) -> None:
@@ -19,8 +20,6 @@ class chessEnv:
         self.board.pop()
         return self.board, False
 
-    
-
 
 def encode_move(move: chess.Move) -> numpy.ndarray:
 #     We represent the policy π(a|s) by a 8 × 8 × 73
@@ -29,10 +28,10 @@ def encode_move(move: chess.Move) -> numpy.ndarray:
 # possible ‘queen moves’ for any piece: a number of squares [1..7] in which the piece will be
 # moved, along one of eight relative compass directions {N, N E, E, SE, S, SW, W, N W }
     letters_arr = ['a','b','c','d','e','f','g','h']
-    from_uci_str = chess.square_name(move.from_square())
+    from_uci_str = chess.square_name(move.from_square)
     from_uci_row = letters_arr.index(from_uci_str[0])
     from_uci_col = int(from_uci_str[1])-1
-    to_uci_str = chess.square_name(move.to_square())
+    to_uci_str = chess.square_name(move.to_square)
     to_uci_row = letters_arr.index(to_uci_str[0])
     to_uci_col = int(to_uci_str[1])-1
     queen_move = (abs(from_uci_col-to_uci_col)==abs(from_uci_row-to_uci_row)) or ((from_uci_col==to_uci_col) or (from_uci_row==to_uci_row))
@@ -55,17 +54,11 @@ def encode_move(move: chess.Move) -> numpy.ndarray:
             else 7
         idx = (distance + 7*dir)-1
     elif promotion_uci_str is None: #knight move
+        mp = [(2, 1), (2, -1), (-2, 1),(-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
         up_distance = to_uci_row - from_uci_row
         right_distance = to_uci_col - from_uci_col
         idx = 56
-        idx = idx + (0 if up_distance==2 and right_distance==1\
-                     else 1 if up_distance==2 and right_distance==-1\
-                        else 2 if up_distance==-2 and right_distance==1\
-                            else 3 if up_distance==-2 and right_distance==-1\
-                                else 4 if up_distance==1 and right_distance==2\
-                                    else 5 if up_distance==1 and right_distance==-2\
-                                        else 6 if up_distance==-1 and right_distance==2\
-                                            else 7)
+        idx = idx + mp.index((up_distance, right_distance))
         
         pass
     else: # promotion
@@ -83,17 +76,50 @@ def encode_move(move: chess.Move) -> numpy.ndarray:
     return arr
 
 def decode_move(array: numpy.ndarray) -> chess.Move:
-    from_col = numpy.where(array != 0)[0][0]
-    from_row = numpy.where(array != 0)[1][0]
+    from_col = numpy.where(array != 0)[0][0] + 1
+    from_row = numpy.where(array != 0)[1][0] + 1
     idx = numpy.where(array != 0)[2][0]
     to_row = 0
     to_col = 0
     promo = None
-    if(idx<64):
-        to_row = 
-        pass
+    if(idx<56):
+        nb = idx
+        distance = (nb % 7) + 1
+        dir = math.floor(nb / 7)
+        # {N, N E, E, SE, S, SW, W, N W }
+        if dir==0 or dir==1:
+            to_row = from_row + distance
+            to_col = from_col if dir==0 else from_col + distance
+        if dir==2 or dir==3:
+            to_row = from_row if dir==2 else from_row-distance
+            to_col = from_col + distance
+        if dir==4 or dir==5:
+            to_row = from_row - distance
+            to_col = from_col if dir==4 else from_col - distance
+        if dir==6 or dir==7:
+            to_row = from_row if dir==6 else from_row+distance
+            to_col = from_col - distance
+        
+    elif(idx>=56 and idx<64):
+        nb = idx-56
+        mp = [(2, 1), (2, -1), (-2, 1),(-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+        pair = mp[nb]
+        up_distance = pair[0]
+        right_distance = pair[1]
+        to_row = from_row+right_distance
+        to_col = from_col+up_distance
+        
+    else:
+        #promo
+        nb = idx-64
+        mult = math.floor(nb/4)
+        add = nb % 4
+        to_row = 8
+        to_col = from_col-1 if mult==0 else from_col if mult==1 else from_col+1
+        promo = chess.ROOK if add==0 else chess.KNIGHT if add==1 else chess.BISHOP if add==2 else chess.QUEEN
+        
         #queen move, no promo
-    return
+    return chess.Move(from_square=((from_col-1)+8*(from_row-1)), to_square=((to_col-1)+8*(to_row-1)), promotion=promo, drop=None)
 
 def get_bitboard(board: chess.Board) -> numpy.ndarray:
     def map_symbol(s: str) -> int:
