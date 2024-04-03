676aa4178d453b7f28ea050457665c6263b9643b
Thomas Inman <Thomas.p.inman@gmail.com>
2024-04-03 17:29:58 -0400

    Additions to conv.py

--- conv.py
+++ conv.py
@@ -1,11 +1,17 @@
+import numpy as np
 import tensorflow.keras.models as models
 import tensorflow.keras.layers as layers
 import tensorflow.keras.utils as utils
 import tensorflow.keras.optimizers as optimizers
+import tensorflow.keras.callbacks as callbacks
 
 class convNet:
-    def __init__(self, inputShape):
+    def __init__(self, inputShape, convSize, convDepth):
+        #@Param inputShape : the shape of thew observation -> transformed board state
+        #@param convSize : the size of the filter (will be the outputy size)
+        #@param convDepth : Number of Convolution layers in the model
         self.inputShape = inputShape
+        self.buildConvNet(convSize, convDepth)
         
     def buildConvNet(self, convSize, convDepth):
         # Define input layers
@@ -15,11 +21,31 @@ class convNet:
         # This creates the hidden layers in the convNet
         x = board_3d
         for _ in range(convDepth):
-            x = layers.Conv2D(filters=convSize, kernel_size=3, padding='same', activation='relu', data_format="channels_first")(x)
+            x = layers.Conv2D(filters=convSize, kernel_size=3, padding='same', activation='relu', data_format="channels_last")(x)
         # The curr size of x is (?, convSize, 8, 8)
         x = layers.Flatten()(x)
         x = layers.Dense(convSize * boardSize, 'relu')(x) # An array of size convSize * boardSize
         
         self.model = models.Model(inputs=board_3d, outputs=x)
-        return self.model
+        self.model.compile(optimizer = optimizers.Adam(5e-4), loss='mean_squared_error')
     
+    def fit(self, train_x, train_y, epochs=100):
+        assert train_x.shape[0] == train_y.shape[0]
+        self.model.fit(train_x, train_y, epochs=epochs)
+        
+    def forward(self, board):
+        #@param board : same dims as inputShape -> representatio of board state
+        print(board.shape)
+        return self.model.predict(board)
+    
+
+net = convNet((8, 8, 14), 76, 3)
+print(net.model.summary())
+
+train_x = np.random.uniform(0, 1, size=(100, 8, 8, 14))
+train_y = np.random.uniform(0, 1, size=(100, 76*8*8))
+print("x: " + str(train_x.shape))
+print("Y: " + str(train_y.shape))
+net.fit(train_x, train_y, epochs=5)
+print(net.forward(np.random.uniform(1, 2, size=(8, 8, 14))))
+
