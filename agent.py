4460cf3ebe63c33c0e99407e115e6586442c1f1d
Ezra Marin <marinezra02@gmail.com>
2024-04-06 14:42:28 -0400

    5

--- agent.py
+++ agent.py
@@ -153,6 +153,6 @@ if __name__ == '__main__':
     env = chessenv.chessEnv(chess.Board())
     dql = DeepQLearning(env, (12, 8, 8), 500, 64, 0.5, .95, 0.2, 0.95)
     # dql.train(100) # test with 100 episodes
-    dql.load("checkpoints\\model200.h5", "checkpoints\\targetModel200.h5", 200)
+    dql.load("checkpoints\\model500.h5", "checkpoints\\targetModel500.h5", 500)
     dql.train(1000) # train for 1000 episodes
     
